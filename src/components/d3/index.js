0b00f480e1c51d885c42c6b574495fefa60e058e
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-09-17 15:12:02 +0530

    Example with Vanilla D3

--- src/components/d3/index.js
+++ src/components/d3/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {  useEffect, useState } from 'react';
 import "./styles.css";
+import BarChart from './BarChart';
+
+const datas = [
+    [10, 30, 40, 20],
+    [10, 40, 30, 20, 50, 10],
+    [60, 30, 40, 20, 30]
+]
+var i = 0;
 
 
 
-class D3Index extends React.Component {
-    render() {
-        return (
-            <>
-                <div className="plot-container">
-                    <h1>Example with D3</h1>
-                </div>
-            </>
-        );
+const D3Index = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        changeData();
+    }, []);
+
+    const changeData = () => {
+        setData(datas[i++]);
+        if (i === datas.length) i = 0;
     }
+
+    return (
+        <>
+            <div className="plot-container">
+                <h1>Example with D3</h1>
+                {/* <button onClick={changeData}>Change Data</button> */}
+                <BarChart width={300} height={300} data={data} />
+            </div>
+        </>
+    );
+
 }
 
 export default D3Index;
