4f67c18a87e3eee2796a9abb2c7b17da77ac46d4
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-09-18 13:21:27 +0530

    Example with high charts

--- src/components/highcharts/index.js
+++ src/components/highcharts/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import "./styles.css";
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
-const options = {
+
+const options1 = {
     title: {
         text: 'Institute Strength '
     },
     chart: {
-        type: 'bar'
+        type: 'column'
     },
     xAxis: {
-        categories: ["Students", "Teachers", "Courses"],
+        categories: ["Maths", "Chemistry", "Physics"],
         title: {
-            text: null
+            text: "Course"
+        }
+    },
+    yAxis: {
+        title: {
+            text: "No. of Students"
         }
     },
     series: [{
@@ -22,17 +28,98 @@ const options = {
     }]
 }
 
+const options2 = {
+    title: {
+        text: 'Fee Status '
+    },
+    chart: {
+        type: 'column'
+    },
+    xAxis: {
+        categories: ["Physics", "Chemistry", "Maths"],
+        title: {
+            text: null
+        }
+    },
+    series: [{
+        name: 'Fees Paid',
+        data: [30, 40, 5],
+    },
+    {
+        name: 'Fees Not Paid',
+        data: [70, 10, 75],
+    },],
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+}
+
+const options3 = {
+    title: {
+        text: 'Students Trend'
+    },
+    chart: {
+        type: 'line'
+    },
+    xAxis: {
+        categories: [2014, 2015, 2016, 2017, 2018, 2019, 2020],
+        title: {
+            text: null
+        }
+    },
+    series: [
+        {
+            name: 'Physics',
+            data: [160, 50, 110, 90, 101, 123, 400],
+        },
+        {
+            name: 'Chemistry',
+            data: [190, 85, 110, 189, 180, 223, 300],
+        },
+        {
+            name: 'Maths',
+            data: [106, 65, 91, 90, 211, 333, 250],
+        },
+    ],
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+}
 
 class HighChartIndex extends React.Component {
     render() {
         return (
             <>
-                <div className="plot-container">
+                <div className="plot-containerhc">
                     <h1>Example with HighCharts</h1>
-                    <HighchartsReact
-                        highcharts={Highcharts}
-                        options={options}
-                    />
+                    <div style={{ width: "40%" }}>
+                        <HighchartsReact
+                            highcharts={Highcharts}
+                            options={options1}
+                        />
+                    </div>
+                    <div style={{ width: "40%" }}>
+                        <HighchartsReact
+                            highcharts={Highcharts}
+                            options={options2}
+                        />
+                    </div>
+                    <div style={{ width: "40%" }}>
+                        <HighchartsReact
+                            highcharts={Highcharts}
+                            options={options3}
+                        />
+                    </div>
                 </div>
             </>
         );
